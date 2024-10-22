a5914f9184fb2ddaa2e15e84910542323e2d63c5
Junior-Knoth <161972203+Junior-Knoth@users.noreply.github.com>
2024-10-22 17:03:41 +0000

    finished hour js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
-
-
+// timer
 function updateTimer() {
     const date = new Date()
     const hours = date.getHours().toString().padStart(2, "0")
@@ -8,7 +7,38 @@ function updateTimer() {
 
     const timer = document.getElementById('timer')
     timer.textContent = `${hours}:${minutes}:${seconds}`
-
 }
 
 setInterval(updateTimer, 1000)
+
+const forms = document.getElementById("forms")
+
+forms:addEventListener("submit",async (e)=>{
+    e.preventDefault()
+
+    try {
+        const city_p = document.getElementById("city-name")
+        const temp = document.getElementById("temp")
+        const humidity = document.getElementById("humidity")
+
+        const city = document.getElementById("weather").value
+
+        const apiKEY = 'ed8e74438a1faf3620054e37423583a9'
+        const apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(city)}&appid=${apiKEY}&units=metric&lang=pt_br`
+
+        const res = await fetch(apiURL)
+        if(!res.ok){
+            throw new Error('Não foi encontrado a cidade!')
+        }
+
+        const data = await res.json()
+
+        city_p.textContent = `${data.name} - ${data.sys.country}`
+        temp.textContent = `Temperatura: ${data.main.temp}ºC`
+        humidity.textContent = `Humidade: ${data.main.humidity}%`
+
+
+    } catch (e) {
+        alert(e.message)
+    }
+})
